51ae3ff8b04bca5387344736686776e7e8801dc3
Cécile Cayèré <cecile.cayere@gmail.com>
2025-12-03 08:32:41 +0100

    2025 - Day 3

--- 2025/day-03/day_3_part_1.py
+++ 2025/day-03/day_3_part_1.py
@@ -2,5 +2,18 @@ import sys
 sys.path.append('../advent-of-code')
 from utils import *
 
-content = get_file_content('2025/day-03/sample.txt')
-content = split_string(content, ",")
+content = get_file_content('2025/day-03/input.txt')
+content = split_string(content, "\n")
+
+total_joltage = 0
+for bank in content:
+    joltage = 0
+    current_joltage = ""
+    for index1 in range(0, len(bank)):
+        for index2 in range(index1 + 1, len(bank)):
+            current_joltage = str(bank[index1]) + (bank[index2])
+            if(int(current_joltage) > joltage):
+                joltage = int(current_joltage)
+    total_joltage = total_joltage + joltage
+
+print(total_joltage)
---
+++ 2025/day-04/README.md
@@ -0,0 +1,7 @@
+# Day 4:
+
+## Part One
+
+## Part Two
+
+Source: https://adventofcode.com/2025/day/4
